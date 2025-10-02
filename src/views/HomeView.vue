1dd2baf598e5686554945abfc401d5c2fb7fe7b0
Excel <excelganagana8@gmail.com>
2025-10-02 17:00:46 +0100

    set up node server and made a register api

---
+++ src/views/HomeView.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="p-6">
+    <h1>Home Page</h1>
+    <p>Welcome to the app 🚀</p>
+  </div>
+</template>
---
+++ src/views/RegisterView.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <h1>Register</h1>
+
+    <input type="email" name="email" placeholder="email" v-model="email">
+    <br>
+    <input type="password" name="password" placeholder="password" v-model="password">
+    <br>
+    <button
+     @click="register"
+    >Register
+
+    </button>
+
+  </div>
+</template>
+
+<script setup>
+import AuthenticationService from '@/services/AuthenticationService'
+import { ref } from 'vue'
+
+let email = ref('')
+let password = ref('')
+
+const register = async () => {
+  try {
+    const res = await AuthenticationService.register({
+      email: email.value,
+      password: password.value
+    })
+    console.log(res.data)
+    alert(res.data.message)
+  } catch (error) {
+    console.error(error)
+    alert('Registration failed')
+  }
+}
+</script>
+
